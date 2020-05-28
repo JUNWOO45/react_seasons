d0df03e383876f092b4ee7bb273b23873b7ab23a
junwoo45 <sinyoorang@gmail.com>
2020-05-28 17:37:32 +0900

    feat: 로딩 스피너 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-// import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import './index.css';
 
 class App extends React.Component {
     constructor(props) {
@@ -28,21 +29,24 @@ class App extends React.Component {
         console.log('componentDidUpdate ::: ');
     }
 
-    render() {
+    renderContent() {
         if(this.state.errorMsg && !this.state.lat) {
             return <div>Error: { this.state.errorMsg }</div>
         }
 
         if(!this.state.errorMsg && this.state.lat) {
-            return <SeasonDisplay lat={ this.state.lat }></SeasonDisplay>
+            return <SeasonDisplay lat={ this.state.lat }></SeasonDisplay>;
         }
 
+        return <Spinner message="내 위치 확인 권한을 허용해주세요."></Spinner>;
+    }
+
+    render() {
         return (
-            <div>
-                <div>로딩중</div>
-                <i className="spinner loading icon"></i>
+            <div className="season-display-border">
+                { this.renderContent() }
             </div>
-        )
+        );
     }
 }
 
